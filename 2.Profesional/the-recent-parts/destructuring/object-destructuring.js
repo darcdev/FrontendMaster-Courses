90efd991a3cb647b6564758dafe237c294368e93
=Diego200013 <diegoandresrojas2000@gmail.com>
2020-05-29 11:21:50 -0500

    Destructuring --Object assignment destructuring

--- 2.Profesional/the-recent-parts/destructuring/object-destructuring.js
+++ 2.Profesional/the-recent-parts/destructuring/object-destructuring.js
@@ -11,3 +11,18 @@ function data() {
 }
 var {a: first, b: second, c: third = 5, ...rest} = data();
 console.log(third);
+
+// if exist variables
+
+function data() {
+  return {
+    a: 3,
+    b: 2,
+    d: 10,
+    f: 9,
+  };
+}
+var tmp;
+var first, second;
+
+tmp = {a: first, b: second} = data();
