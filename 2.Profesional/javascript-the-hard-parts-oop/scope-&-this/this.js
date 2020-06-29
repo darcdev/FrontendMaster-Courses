ac664fc1f7eaa83989038a8073b244c9b8e68213
darcdev <diegoandresrojas2000@gmail.com>
2020-06-29 13:12:35 -0500

    scope-&-this --dynamic this

---
+++ 2.Profesional/javascript-the-hard-parts-oop/scope-&-this/this.js
@@ -0,0 +1,17 @@
+function UserCreator(name, score) {
+  this.name = name;
+  this.score = score;
+}
+UserCreator.prototype.increment = function () {
+  this.score++;
+  //this -> user#
+  //user#.score++
+};
+UserCreator.prototype.loggin = function () {
+  console.log('loggedin');
+};
+
+const user1 = new userCreator('Diego', 10);
+console.log(user1);
+console.log(user1.increment());
+console.log(user1);
