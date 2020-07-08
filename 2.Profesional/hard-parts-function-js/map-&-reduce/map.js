f693fb3066fa03f55e24392dbffa60f822fed44a
darcdev <diegoandresrojas2000@gmail.com>
2020-07-08 11:24:46 -0500

    high-order-functions --map & reduce

--- 2.Profesional/hard-parts-function-js/map-&-reduce/map.js
+++ 2.Profesional/hard-parts-function-js/map-&-reduce/map.js
@@ -1,1 +1,15 @@
 //folk tend to tell coyyArrayAndManipulate as Map
+
+const map = function (array, instruction) {
+  let output = [];
+  for (let i = 0; i < array.length; i++) {
+    output.push(instruction(array[i]));
+  }
+  return output;
+};
+
+const instruction = (input) => input * 2;
+
+const result = map([1, 2, 3], instruction);
+
+console.log(result);
---
+++ 2.Profesional/hard-parts-function-js/map-&-reduce/reduce.js
@@ -0,0 +1,22 @@
+//reduce two values to one
+
+const reduce = function (array, instruction, initialValue) {
+  let total = initialValue;
+  for (let i = 0; i < array.length; i++) {
+    if (!initialValue && i == 0) {
+      total = 0;
+    }
+    total = instruction(array[i], total);
+  }
+  return total;
+};
+
+const result = reduce(
+  [1, 2, 3],
+  function (value, total) {
+    return total + value;
+  },
+  0
+);
+
+console.log(result);
